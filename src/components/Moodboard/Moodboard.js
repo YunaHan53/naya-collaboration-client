a967fbde0077c18d730bd0bbd384207f0b2babab
YunaH <YunaH503@gmail.com>
2020-10-18 21:31:43 -0400

    Completed Moodboard page with all sections
    Added furniture keywords, imagery & sketch sections with styling.

--- src/components/Moodboard/Moodboard.js
+++ src/components/Moodboard/Moodboard.js
@@ -1,8 +1,69 @@
 import React from 'react'
+import StyleKeywordsImage from './style-keywords.png'
+import Image from './image.png'
+import SketchImage from './sketch.png'
+import DepositImage from './deposit.png'
 
-export class Moodboard extends React.Component {
+const Style = () => {
+  return (
+    <div>
+      <h5>Furniture Style</h5>
+      <img className="style-keywords-image" src={StyleKeywordsImage} alt="Style Keywords" />
+    </div>
+  )
+}
+
+const ImageCard = ({ imagePath, key }) => {
+  return (
+    <div key={key}>
+      <img className="caption-image" src={imagePath}></img>
+    </div>
+  )
+}
+
+const Sketch = () => {
+  return (
+    <div>
+      <h5>Your Sketch</h5>
+      <img className="sketch-image" src={SketchImage} alt="Sketch" />
+    </div>
+  )
+}
+
+const Deposit = () => {
+  return (
+    <div>
+      <img className="deposit-image" src={DepositImage} alt="Deposit" />
+    </div>
+  )
+}
+
+class Moodboard extends React.Component {
   render () {
-    return (<div>123</div>)
+    const imageArr = [Image, Image, Image, Image]
+
+    return (
+      <div className="container">
+        <Style />
+        <h5>Imagery</h5>
+        <div className="image-container">
+          <br /><br />
+          {imageArr.map((img, i) => {
+            return <ImageCard key={i} imagePath={img}/>
+          })
+          }
+        </div>
+        <div className="image-container">
+          <br /><br />
+          {imageArr.map((img, i) => {
+            return <ImageCard key={i} imagePath={img}/>
+          })
+          }
+        </div>
+        <Sketch />
+        <Deposit />
+      </div>
+    )
   }
 }
 
