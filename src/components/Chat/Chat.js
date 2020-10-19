6b317006e0d8b55550690124b65d9e365899216e
YunaH <YunaH503@gmail.com>
2020-10-18 22:13:37 -0400

    Completed sample chatbox in Chat component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import 'react-chatbox-component/dist/style.css'
+import { ChatBox } from 'react-chatbox-component'
+
+const messages = [
+  {
+    'text': 'Hello there',
+    'id': '1',
+    'sender': {
+      'name': 'Ironman',
+      'uid': 'user1',
+      'avatar': 'https://data.cometchat.com/assets/images/avatars/ironman.png'
+    }
+  },
+  {
+    'text': 'Hi Mr. Stark',
+    'id': '2',
+    'sender': {
+      'name': 'Spiderman',
+      'uid': 'user2',
+      'avatar': 'https://data.cometchat.com/assets/images/avatars/spiderman.png'
+    }
+  },
+  {
+    'text': 'Hello Spiderman, how are you today?',
+    'id': '3',
+    'sender': {
+      'name': 'Ironman',
+      'uid': 'user1',
+      'avatar': 'https://data.cometchat.com/assets/images/avatars/ironman.png'
+    }
+  }
+]
 
 export class Chat extends React.Component {
   render () {
     return (
-      <div>abc</div>
+      <div className="container" style={{ maxWidth: '800px', paddingTop: '100px', paddingBottom: '50px' }}>
+        <ChatBox
+          messages={messages}
+        />
+      </div>
     )
   }
 }
